1a9a3df46d3ad6f741c2aef070ab05090edc689a
goldiefever <mgoldhaberfiebert2028@kehillahstudent.org>
2025-04-30 12:24:09 -0700

    done! (and I added some cool stuff 😀)

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import json
 
 # read `expenses.json`
+with open ("expenses.json", "r") as f:
+    stores_and_prices = json.load(f)
+
+username = input("What is your name?")
 
 # get and print total "price" for all expenses at the "pet store"
+def expenses_per_store(store):
+    expenses = 0
+    for key in stores_and_prices:
+        if key == store:
+            for item in range(len(((stores_and_prices[store])))):
+                expenses += stores_and_prices[store][item]["price"]
+        else:
+            pass
+
+    print(f"You have spent ${expenses} at the {store}")
+    if expenses >= 100:
+        print("Time to start saving...")
+    else:
+        print(f"Wow {username}, you are a good saver! 💰💰💰")
+
+expenses_per_store(input("Which store would you like to check your expenses at?\nThe options are pet store, grocery store, and office supply store.\n"))
+
